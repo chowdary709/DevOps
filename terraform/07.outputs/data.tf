00b3e5d4d59e1de51d1bbd9c303e37f6dbc0a2fb
chowdary709 <chandrahari321@gmail.com>
2024-06-15 18:50:45 +0530

    automated commit #2750

--- terraform/07.outputs/data.tf
+++ terraform/07.outputs/data.tf
@@ -3,3 +3,9 @@ data "aws_ami" "ami" {
   most_recent = true
   name_regex  = "Centos-8-DevOps-Practice"  
 }
+
+# how to get default vpc in data source
+
+data "aws_vpc" "default" {
+  default = true
+}
--- terraform/07.outputs/outputs.tf
+++ terraform/07.outputs/outputs.tf
@@ -1,3 +1,9 @@
 output "ami_id" {
   value = data.aws_ami.ami.id
 }
+
+# how to get default vpc id
+
+output "vpc_id" {
+  value = data.aws_vpc.default
+}
